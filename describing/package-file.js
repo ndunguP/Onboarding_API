8bf37080ea04cb94b94452a8c385cad723b44793
ndunguP <peter.ndunguoz50@gmail.com>
2024-09-14 17:05:37 +0300

    upadting the JSON file

---
+++ describing/package-file.js
@@ -0,0 +1,23 @@
+{
+  "name": "Onboaring API",
+  "version": "1.0.0",
+  "description": "Onboarding API for BigCo, Inc.",
+  "main": "app.js",
+  "scripts": {
+  "test": "echo \"Error: no test specified\" && exit 1"
+},
+"keywords": [
+"onboarding",
+"company",
+"account",
+"activity",
+"api"
+],
+"author": "Mike Amundsen",
+"license": "MIT",
+"assets" : {
+   "diagram" : "OnboardingAPI.png",
+   "alps" : "OnboardingAPI.json",
+   "apiDocs" : "Onboarding.md"
+ }
+}
